8f4ba23e559c0cbd106403a22c571bfb09ba51c4
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-29 14:21:55 +0300

    feat: desing && logic

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="bg-secondary">
+    <q-img src="background.jpg" class="fit absolute"/>
     <q-page-container>
       <router-view />
     </q-page-container>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,60 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center text-white justify-center items-center">
+
+    <div class="row">
+      <q-input placeholder="Counter" color="warning" filled input-class="text-center text-white text-h6"
+        v-model="data.name" />
+
+    </div>
+
+    <div class="row full-width ">
+      <div class="col text-center">
+        <q-btn outline round color="warning" size="lg" icon="remove" @click="decreaseCounter()"/>
+      </div>
+      <div class="col text-center text-h2">{{ data.counter }}</div>
+      <div class="col text-center">
+        <q-btn outline round color="warning" size="lg" icon="add" @click="increaseCounter"/>
+      </div>
+    </div>
+
+    <div class="row full-width">
+      <div class="col text-center">
+        <q-btn round color="warning" icon="restart_alt" size="xl" padding="md" @click="resetCounter">
+          <q-tooltip>
+            Reset Counter
+          </q-tooltip>
+        </q-btn>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { reactive } from 'vue'
+
+const data = reactive({
+  counter: 0,
+  name: "",
+});
+
+const increaseCounter = () => {
+  data.counter++;
+}
+
+const decreaseCounter = () => {
+  if(data.counter >0)
+    data.counter--;
+}
+
+const resetCounter = () => {
+  data.counter = 0;
+}
+
 </script>
+
+<style lang="scss">
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
